7a166b5d6d556f6145fd1f9ec0f7dfa7a0e74aff
vicentegonz <vgonzalez4@uc.cl>
2023-04-19 03:24:03 +0800

    new

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template
 import random
-import cv2
+#import cv2
 from pyzbar.pyzbar import decode
 import socket
 
@@ -36,11 +36,11 @@ def manual_display_keypad(type):
 def acceso(input):
     granted = random.choice([True, False])
     parsed = input.split(",")
-    img = cv2.imread("static/img/frame.png")
-    qrData = decode(img)
-    if len(qrData) > 0:
-        binary = bin(int.from_bytes(qrData[0].data, byteorder='big'))
-        print(binary)
+    #img = cv2.imread("static/img/frame.png")
+    #qrData = decode(img)
+    #if len(qrData) > 0:
+        #binary = bin(int.from_bytes(qrData[0].data, byteorder='big'))
+        #print(binary)
     data = {
         "cel": parsed[0],
         "ci": parsed[1],
@@ -48,6 +48,6 @@ def acceso(input):
         "x": random.choice([0, 1]),
         "MAC": "00:00:00:00:00:00",
         "XXX": "elemento XXX o formato pdf417/qr",
-        "binary": binary
+        #"binary": binary
     }
     return render_template("acceso.html", granted=granted, data=data)
